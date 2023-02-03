d7863bbefc664bc33236d00be2055b8a867e4468
yaw.o.k <obengky72@gmail.com>
2023-02-03 23:58:23 +0000

    Add bubble sort

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(list):
+    n = len(list)
+    for item in range(1, n - 1):
+        for jtem in range(n - item):
+            if list[jtem] > list[jtem + 1]:
+                temp = list[jtem]
+                list[jtem] = list[jtem + 1]
+                list[jtem + 1] = temp
+    return list
+
+numbers = [4, 5, 2, 66, 44, 1, 3]
+print(bubble_sort(numbers))
