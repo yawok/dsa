806a7cfc2e02d889e257951e7678eb941ce158d8
yaw.o.k <obengky72@gmail.com>
2023-02-04 00:05:28 +0000

    Add more bubble sort tests

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,3 +1,6 @@
+# 03/02/23
+# bubble sort algorithm 
+
 def bubble_sort(list):
     n = len(list)
     for item in range(1, n - 1):
@@ -8,5 +11,12 @@ def bubble_sort(list):
                 list[jtem + 1] = temp
     return list
 
+
 numbers = [4, 5, 2, 66, 44, 1, 3]
-assert(bubble_sort(numbers) == [1, 3, 2, 3, 4, 5, 44, 66])
+numbers1 = [4, 5]
+numbers2 = [10, 10, 10, 10, 10]
+numbers3 = [100000, 33.4, 1, -47, -3]
+assert bubble_sort(numbers) == [1, 2, 3, 4, 5, 44, 66]
+assert bubble_sort(numbers1) == [4, 5]
+assert bubble_sort(numbers2) == [10, 10, 10, 10, 10]
+assert bubble_sort(numbers3) == [-47, -3, 1, 33.4, 100000]
